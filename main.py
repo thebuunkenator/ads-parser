732da9299572d921ea9d0cf2ade32cd61e0ecefa
Erik Buunk <erik.buunk@gmail.com>
2015-01-02 13:05:16 +0100

    simplify retrieval of xml parameters

--- main.py
+++ main.py
@@ -3,6 +3,18 @@ import urllib2
 import os
 import xml.etree.ElementTree as ET
 
+def xml_value (record, parameter):
+    if record.find(parameter) != None:
+        value = record.find(parameter).text.encode('utf-8')
+        print parameter + ": "  + value
+    else:
+        print parameter + ": not found."
+
+def xml_multiple_values (record, parameter):
+    print parameter + 's:'
+    for item in record.iter(parameter):
+        print "\t" + item.text.encode('utf-8')
+
 ## Parse XML
 def parse_xml (fileName):
 
@@ -15,39 +27,27 @@ def parse_xml (fileName):
     print root.tag
     print root.attrib
 
-    # for country in root:
-    #     print country.attrib
-    #
-    #     rank = country.find('rank').text
-    #     name = country.get('name').text
-    #     print "name: " +  name
-    #     print "rank: " + rank
-    #
-    #     for neighbor in root.iter('neighbor'):
-    #         print neighbor.attrib
-
-
     for record in root:
 
-
         print
-        title = record.find('title').text.encode('utf-8')
-        print "========================================"
-        bibcode = record.find('bibcode').text.encode('utf-8')
+        print "--------------------------------------------------------------------------"
+        xml_value(record, 'title')
+        print "--------------------------------------------------------------------------"
+        xml_value(record, 'bibcode')
 
-        print record.attrib
-        print "title: " +  title
-        print "bibcode: " + bibcode
+        #print record.attrib
 
-        for author in record.iter('author'):
-            print "\t" + author.text.encode('utf-8')
+        xml_value(record, 'affiliation')
+        xml_value(record, 'journal')
+        xml_value(record, 'volume')
+        xml_value(record, 'pubdate')
+        xml_value(record, 'page')
+        xml_value(record, 'url')
+        xml_value(record, 'score')
+        xml_value(record, 'citations')
 
-    # for record in root:
-    #     print record.tag , record.attrib
-    #     for author in record
-    #         print record.find('author').text
-    #     print record.find('bibcode').text
-    #     print record.find('title').text
+        xml_multiple_values(record, 'author')
+        xml_multiple_values(record, 'keyword')
 
 
 ## download URL
@@ -79,15 +79,24 @@ def dowload_url (url, fileName):
 baseURL = "http://adsabs.harvard.edu/cgi-bin/nph-abs_connect?db_key=AST&db_key=PRE&qform=AST&arxiv_sel=astro-ph&arxiv_sel=cond-mat&arxiv_sel=cs&arxiv_sel=gr-qc&arxiv_sel=hep-ex&arxiv_sel=hep-lat&arxiv_sel=hep-ph&arxiv_sel=hep-th&arxiv_sel=math&arxiv_sel=math-ph&arxiv_sel=nlin&arxiv_sel=nucl-ex&arxiv_sel=nucl-th&arxiv_sel=physics&arxiv_sel=quant-ph&arxiv_sel=q-bio&sim_query=YES&ned_query=YES&adsobj_query=YES&aut_logic=OR&obj_logic=OR&object=&start_mon=&ttl_logic=OR&title=&txt_logic=OR&text=&start_nr=1&jou_pick=ALL&ref_stems=&data_and=ALL&group_and=ALL&start_entry_day=&start_entry_mon=&start_entry_year=&end_entry_day=&end_entry_mon=&end_entry_year=&min_score=&sort=SCORE&aut_syn=YES&ttl_syn=YES&txt_syn=YES&aut_wt=1.0&obj_wt=1.0&ttl_wt=0.3&txt_wt=3.0&aut_wgt=YES&obj_wgt=YES&ttl_wgt=YES&txt_wgt=YES&ttl_sco=YES&txt_sco=YES&version=1"
 
 parameters = {
-"author" : "de+mink",
-"data_type" : "XML", #possible values:
+"author" : "",
+"data_type" : "",
 "start_year" : "",
 "end_year" : "",
-"nr_to_return" : "1000"
+"nr_to_return" : "",
+"query_type" : ""
 }
 
+parameters['author'] = "de+mink"
+parameters['nr_to_return'] = 1000
+parameters['data_type'] = "XML"
+#parameters['start_year'] =2012
+#parameters['end_year'] =2012
+#parameters['query_type'] = "CITES"
 
 url = baseURL + "&" + urllib.urlencode(parameters)
+#print url
+
 fileName = "adw.xml"
 
 
