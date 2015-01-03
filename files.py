e0d575fe04ba8f054f2c497c8fe79579e09c1829
Erik Buunk <erik.buunk@gmail.com>
2015-01-03 21:45:14 +0100

    directory and files are created/added when the don't exist

--- files.py
+++ files.py
@@ -1,30 +1,27 @@
 import urllib
 import urllib2
+import os
 
 #Constants
 REFRESH = False # if set to False: do not  download files (if they are already downloaded)
 XML_PATH = "./xml/"
 
+
+def ensure_dir(f):
+    d = os.path.dirname(f)
+    if not os.path.exists(d):
+        os.makedirs(d)
+
 def dowload_url (url, fileName):
     """Downloads URL into a local file"""
 
-    if REFRESH == True:
-        #TODO: check if directory exists and if not create it
+    ensure_dir(XML_PATH)
+    fileComplete = XML_PATH + fileName
+
+    #load data if refresh is on or file does not exist
+    if (REFRESH) or not (os.path.exists(fileComplete)):
         ads_data = urllib2.urlopen(url)
-
-        # Get all data
-        #print "Retrieving data from ADS"
-        # print ">>> Downloading: " + url
         html = ads_data.read()
-
-        #open the fileName for writing
-        #print "Writing file " + XML_PATH + fileName
-        fh = open(XML_PATH + fileName, "w")
-
+        fh = open(fileComplete, "w")
         fh.write(html)
-
         fh.close()
-
-        #print "Finished"
-    #else:
-    #    print ">>> Skipping download: " + url
